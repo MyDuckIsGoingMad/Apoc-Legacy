5dfedf2f061b20d72bfa9c7977a2663652be0d2b
Vladimir <vladimir@retech.online>
2026-01-02 16:55:02 +0700

    Cleanup libs. Add run.sh for macos and linux

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -Xms512m -Xmx1024m --add-opens java.desktop/sun.awt=ALL-UNNAMED --add-opens java.desktop/sun.java2d=ALL-UNNAMED -jar haven.jar legacy.havenandhearth.com -r ./res -v script
